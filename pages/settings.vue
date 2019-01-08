f542136a68ccf2dd8da573e597a99bea33da5281
Mario Noble <marionoble@Marios-MacBook-Air.local>
2019-01-07 18:51:51 -0800

    refactor using flex

--- pages/settings.vue
+++ pages/settings.vue
@@ -6,41 +6,41 @@
     <span slot="header-title">Settings</span>
     </GeneralHeader>
 
-    <main class="settings-area min-h-screen max-w-sm mx-auto">
+    <main class="settings-area min-h-screen max-w-sm mx-auto p-4">
 
       <!-- node info section -->
-      <section class="node-info-area box">
-        <div class="node-info flex justify-between">
-          <h2>iMyn node 63</h2>
+      <section class="node-info-area my-4">
+        <div class="node-info mb-4 flex justify-between">
+          <h2 class="font-light">iMyn node 63</h2>
           <div>
-            <input type="checkbox" name="toggle-test-net" value="yes">Test net</input>
+            <input type="checkbox" name="toggle-test-net" value="yes" checked> Test net</input>
           </div>
         </div>
-        <div class="firmware flex justify-between">
+        <div class="firmware mt-4 flex justify-between">
           <div>
             <p>Firmware version 0.6.5.2</p>
-            <input type="checkbox" name="auto-update" value="yes">auto update</input>
+            <input class="my-2" type="checkbox" name="auto-update" value="yes"> auto update</input>
           </div>
-          <button class="node-update-action bg-none border-none disabled">Update</button>
+          <button class="node-update-action opacity-50 cursor-not-allowed">Update</button>
         </div>
         
       </section>
 
       <!-- current time section -->
-      <section class="current-time-area flex justify-between box">
+      <section class="current-time-area flex justify-between my-8">
         <div>
           <h3 class="title">Current time</h3>
-          <p class="time-utc">14:32:19 UTC</p>
+          <p class="time-utc mt-4">14:32:19 UTC</p>
         </div>
         <button class="change-btn bg-transparent border-none">Change</button>
       </section>
 
       <!-- satellites -->
-      <section class="satellites-section box">
+      <section class="satellites-section">
 
-          <h3 class="sat-title font-normal text-center">GPS Satellites</h3>
+          <h3 class="sat-title font-normal text-xl">GPS Satellites</h3>
 
-          <div class="satellite-list flex justify-center">
+          <div class="satellite-list mt-4 flex justify-start">
 
             <SatelliteItem>
               <span slot="satellite-id">1A</span>
@@ -59,14 +59,14 @@
 
           </div>
 
-          <div class="location-coords text-lg text-center">
-            <p>36°05'44.7"N 115°10'33.8"W</p>
-            <button class="mt-4 uppercase text-center">Retry</button>
+          <div class="location-coords text-lg flex justify-between items-center">
+            <div class="font-regular">36°05'44.7"N 115°10'33.8"W</div>
+            <button class="block uppercase text-center border-none">Retry</button>
           </div>
 
       </section>
 
-      <form class="wifi-choice box" action="#">
+      <form class="wifi-choice" action="#">
         <label for="wifi-network">Wifi network</label>
           <select name="wifi-network" id="wifi-network">
               <option value="john" selected="selected">Johns Network</option>
@@ -76,7 +76,7 @@
           </select>
       </form>
 
-      <nuxt-link to="/summary" class="wizard-button bg-basic btn btn-primary box">Next</nuxt-link>
+      <nuxt-link to="/summary" class="block wizard-button btn btn-primary pin-b mt-8 w-32 mx-auto">Next</nuxt-link>
 
     </main>
   
